63bc56ab5c7ddff5980aab3d08538a850d7ff9e7
SNachiappan1987 <s.nachiappan@perficient.com>
2018-12-26 17:56:39 +0530

    Test Commit

--- src/main/java/io/pivotal/pal/tracker/ApplicationController.java
+++ src/main/java/io/pivotal/pal/tracker/ApplicationController.java
@@ -1,15 +1,28 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class ApplicationController {
 
+
+    @Value("${WELCOME_MESSAGE}")
+    String welcomeMessage ;
+
+    public ApplicationController(String welcomeMessage) {
+        this.welcomeMessage = welcomeMessage;
+    }
+
+    public ApplicationController() {
+        this.welcomeMessage = welcomeMessage;
+    }
+
+
     @GetMapping("/")
-    public String getMap(){
-        return "hello";
+    public String sayHello(){
+        return welcomeMessage;
     }
 }
